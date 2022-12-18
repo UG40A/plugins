39a9276868a0bb5db519615786962e024c94012c
UG40A <73245471+UG40A@users.noreply.github.com>
2022-12-18 15:14:53 +0600

    Update download.sh

--- download.sh
+++ download.sh
@@ -38,9 +38,9 @@ download_xray() {
 
 download_singbox() {
   mkdir_libs "app_singbox/libs"
-  curl -Lso singbox.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v1.1-rc1/sing-box-1.1-rc1-android-arm64.tar.gz"
+  curl -Lso singbox.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v1.1.1/sing-box-1.1.1-android-arm64.tar.gz"
   unzip_singbox arm64-v8a
-  curl -Lso singbox.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v1.1-rc1/sing-box-1.1-rc1-android-amd64.tar.gz"
+  curl -Lso singbox.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v1.1.1/sing-box-1.1.1-android-amd64.tar.gz"
   unzip_singbox x86_64
 }
 
